029e79eac1c1976af30f7c31f99a4933139fb0fb
joaoMarinho94 <joaopaulim94@gmail.com>
2022-02-08 21:58:12 -0300

    feat: add refresh token

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import { useAuthContext } from '../context/AuthContext';
+import { api } from '../services/api';
 
 export default function Dashboard(): JSX.Element {
   const { user } = useAuthContext();
+
+  useEffect(() => {
+    api.get('/me').then(({ data }) => {
+      console.log('data: ', data);
+    });
+  }, []);
 
   return <h1>dashboard: {user?.email}</h1>;
 }
